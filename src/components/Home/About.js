f7f16b6ff75746b16acfe68dfbd3bb401a3dee7a
kagame8732 <kagame8732@gmail.com>
2022-05-18 10:03:08 +0200

    update

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -2,7 +2,6 @@ import img2 from "../images/about/img2.png";
 const About = () => {
   return (
     <div id="about-us" class="about">
-      <h1>About</h1>
       <section class="about-us">
         <div class="all-1">
           <div class="container-1">
